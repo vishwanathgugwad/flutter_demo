d25b53ed5ebf254e3af5efc6f38bacac2733f8dd
vishwanathgugwad <vishwanath.gugwad@gmail.com>
2021-09-13 22:45:50 +0530

    added image slider to home page

---
+++ lib/global.dart
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+final  urlImages = [
+  'https://images.unsplash.com/photo-1565018054866-968e244671af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1616&q=80',
+  'https://images.unsplash.com/photo-1596176530529-78163a4f7af2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1099&q=80',
+  'https://images.unsplash.com/photo-1596176530529-78163a4f7af2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1099&q=80',
+  'https://images.unsplash.com/photo-1596796679119-7cf1a9e5448b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+  'https://images.unsplash.com/photo-1596796679119-7cf1a9e5448b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+];
--- lib/pages/blogs_page.dart
+++ lib/pages/blogs_page.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_demo/widgets/drawer.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:flutter_demo/pages/images_url.dart';
-
+import '../global.dart';
 
 class BlogsPage extends StatefulWidget {
   const BlogsPage({Key? key}) : super(key: key);
@@ -15,18 +14,8 @@ class BlogsPage extends StatefulWidget {
 
 class _BlogsPageState extends State<BlogsPage> {
 
-
-
   int activeIndex = 0;
 
-  final List<String> urlImages = [
-
-    'https://images.unsplash.com/photo-1565018054866-968e244671af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1616&q=80',
-    'https://images.unsplash.com/photo-1596176530529-78163a4f7af2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1099&q=80',
-    'https://images.unsplash.com/photo-1596176530529-78163a4f7af2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1099&q=80',
-    'https://images.unsplash.com/photo-1596796679119-7cf1a9e5448b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
-    'https://images.unsplash.com/photo-1596796679119-7cf1a9e5448b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
-  ];
   @override
   Widget build(BuildContext context) {
 
